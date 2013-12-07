18f39a7299111cef102092377b00a818f0e1607a
Arnaud Grausem <arnaud.grausem@gmail.com>
2013-12-07 14:12:27 +0100

    fix artistcountry for beets 1.3

--- artistcountry.py
+++ artistcountry.py
@@ -1,5 +1,4 @@
 from beets.plugins import BeetsPlugin
-from beets import mediafile
 from musicbrainzngs.musicbrainz import get_artist_by_id
 
 class CountryPlugin(BeetsPlugin):
@@ -8,7 +7,7 @@ class CountryPlugin(BeetsPlugin):
 def memoize_artist(f):
     cache = {}
     def memf(item):
-        artist_id = item.record['mb_artistid']
+        artist_id = record['mb_artistid']
         if artist_id not in cache:
             cache[artist_id] = f(item)
         return cache[artist_id]
@@ -17,7 +16,7 @@ def memoize_artist(f):
 @CountryPlugin.template_field('artist_country')
 @memoize_artist
 def _tmpl_country(item):
-    artist_item = get_artist_by_id(item.record['mb_artistid'])
+    artist_item = get_artist_by_id(record['mb_artistid'])
     artist_country = artist_item['artist'].get('country', '')
     if not artist_country:
         print "No country for %s" % artist_item['artist']['name']
