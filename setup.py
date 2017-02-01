4cece31ba52493730a1e816ceef18d802c315187
v-eye <arnaud.grausem@unistra.fr>
2017-02-01 12:26:31 +0100

    add new minor version :
    	* add MIT license

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from setuptools import setup
 
 setup(
     name='beets-artistcountry',
-    version='0.1.1',
+    version='0.1.2',
     namespace_packages=['beetsplug'],
     packages=['beetsplug'],
     author='Arnaud Grausem',
     author_email='arnaud.grausem@gmail.com',
+    license='MIT',
     description='Beets plugin to retrieve the country of an artist from musicbrainz',
     long_description=open('README.rst').read(),
     url='https://github.com/agrausem/beets-artistcountry',
