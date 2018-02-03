efd73875ac0ad6db6af3f3cc99ee1c43fd541f96
v-eye <arnaud.grausem@unistra.fr>
2018-02-03 17:55:30 +0100

    python3 compat

--- beetsplug/artistcountry.py
+++ beetsplug/artistcountry.py
@@ -19,6 +19,6 @@ def _tmpl_country(item):
     artist_item = get_artist_by_id(item['mb_artistid'])
     artist_country = artist_item['artist'].get('country', '')
     if not artist_country:
-        print "No country for %s" % artist_item['artist']['name']
+        print("No country for %s" % artist_item['artist']['name'])
         return ''
     return artist_country.lower()
