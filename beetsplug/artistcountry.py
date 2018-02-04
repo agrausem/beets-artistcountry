8e8177a357d54fa6d1aac769071d9dcfb623b146
v-eye <arnaud.grausem@unistra.fr>
2018-02-04 15:54:43 +0100

    get country from area it country not set directly

--- beetsplug/artistcountry.py
+++ beetsplug/artistcountry.py
@@ -1,5 +1,5 @@
 from beets.plugins import BeetsPlugin
-from musicbrainzngs.musicbrainz import get_artist_by_id
+from musicbrainzngs.musicbrainz import get_artist_by_id, get_area_by_id
 
 class CountryPlugin(BeetsPlugin):
     pass
@@ -13,12 +13,33 @@ def memoize_artist(f):
         return cache[artist_id]
     return memf
 
+
 @CountryPlugin.template_field('artist_country')
 @memoize_artist
 def _tmpl_country(item):
     artist_item = get_artist_by_id(item['mb_artistid'])
-    artist_country = artist_item['artist'].get('country', '')
-    if not artist_country:
-        print("No country for %s" % artist_item['artist']['name'])
-        return ''
-    return artist_country.lower()
+    artist = artist_item['artist']
+    country = artist.get('country', '')
+    if not country:
+        try:
+            country = _country_from_area(artist['area'])
+        except:
+            print("No country for %s" % artist['name'])
+            return ''
+    return country.lower()
+
+
+def _country_from_area(area):
+    countries = _find_top_area(area)
+    return countries[0]
+
+
+def _find_top_area(area):
+    new_area = get_area_by_id(area['id'], includes=['area-rels'])
+    new_area = [
+        a['area'] for a in new_area['area']['area-relation-list']
+        if a.get('direction', '') == 'backward'
+    ]
+    if not new_area:
+        return area['iso-3166-1-code-list']
+    return _find_top_area(new_area[0])
